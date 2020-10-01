d3981bd3da5ed3b63e177478870603eebb603194
Cristian <ccuevas3410@gmail.com>
2020-09-30 20:22:21 -0400

    Started the Customer Login page

--- client/src/components/access_pages/login/customer_login/CustomerLogin.js
+++ client/src/components/access_pages/login/customer_login/CustomerLogin.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Container,Form,Button} from 'react-bootstrap';
+import {Container,Form,Button, Row,Col} from 'react-bootstrap';
+import Logo from "../../../../assets/logo.png";
 import './CustomerLogin.css'
 
 export default class CustomerLogin extends React.Component {
@@ -7,7 +8,55 @@ export default class CustomerLogin extends React.Component {
 
   render() {
     return (
-      <div>Customer Login Page</div>
+
+<div>
+      <Container>
+  <Row>
+    <Col>
+    <div className="info">
+    <img src={Logo} id="logo" alt="Smiley face"></img>
+     <h1>  Delivery food that you will love.</h1> 
+      </div>
+      
+      </Col>
+    <Col xs={8}><div className="form">
+          <h5> Sign in with your 
+ChefItUp account</h5>
+      <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+       
+      </Form.Group>
+    
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+     
+      <Button id="sign-in-button" variant="primary" type="submit">
+        Sign in
+      </Button>
+
+
+    
+      <Form.Text >
+     Dont have an account? <a>Sign up</a>
+    </Form.Text>
+    <Form.Text >
+     Forgot password?
+    </Form.Text>
+    </Form>
+    </div></Col>
+  </Row>
+</Container>
+
+
+</div>
+
+        
+
+ 
     );
   }
 }
